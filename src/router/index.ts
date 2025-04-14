cc2f6da5ce78b5f507ae3f8c775a691f6154ac3a
Claystation <c.wijnekus@gmail.com>
2025-04-14 10:30:10 +0200

    Complete reset

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,168 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { useAuthStore } from "@/stores/auth";
-
-import SigninView from "@/views/Authentication/SigninView.vue";
-
-import CampyearIndex from "@/views/Campyear/Index.vue";
-import CampyearCreate from "@/views/Campyear/Create.vue";
-import CampyearEdit from "@/views/Campyear/Edit.vue";
-
-import UserIndex from "@/views/User/Index.vue";
-import UserCreate from "@/views/User/Create.vue";
-import UserEdit from "@/views/User/Edit.vue";
-
-import VolunteerIndex from "@/views/Volunteer/Index.vue";
-import VolunteerCreate from "@/views/Volunteer/Create.vue";
-import VolunteerEdit from "@/views/Volunteer/Edit.vue";
-
-import CampRegistationIndex from "@/views/CampRegistration/Index.vue";
-import CampRegistrationEdit from "@/views/CampRegistration/Edit.vue";
-
-import Dashboard from "@/views/Dashboard/Dashboard.vue";
-import SettingsView from "@/views/Pages/SettingsView.vue";
-
-const routes = [
-  {
-    path: "/",
-    name: "dashboard",
-    component: Dashboard,
-    meta: {
-      title: "Dashboard",
-    },
-  },
-  {
-    path: "/campyear",
-    name: "campyear",
-    component: CampyearIndex,
-    meta: {
-      title: "Kampjaren",
-    },
-  },
-  {
-    path: "/campyear/create",
-    name: "campyearCreate",
-    component: CampyearCreate,
-    meta: {
-      title: "Kampjaar aanmaken",
-    },
-  },
-  {
-    path: "/campyear/:year",
-    name: "campyearEdit",
-    component: CampyearEdit,
-    meta: {
-      title: "Kampjaar wijzigen",
-    },
-    props: true,
-  },
-  {
-    path: "/user",
-    name: "user",
-    component: UserIndex,
-    meta: {
-      title: "Gebruikers",
-    },
-  },
-  {
-    path: "/user/create",
-    name: "userCreate",
-    component: UserCreate,
-    meta: {
-      title: "Gebruiker aanmaken",
-    },
-  },
-  {
-    path: "/user/:id",
-    name: "userEdit",
-    component: UserEdit,
-    meta: {
-      title: "Gebruker wijzigen",
-    },
-    props: true,
-  },
-  {
-    path: "/volunteer",
-    name: "volunteer",
-    component: VolunteerIndex,
-    meta: {
-      title: "Medewerkers",
-    },
-  },
-  {
-    path: "/volunteer/create",
-    name: "volunteerCreate",
-    component: VolunteerCreate,
-    meta: {
-      title: "Medewerker aanmaken",
-    },
-  },
-  {
-    path: "/volunteer/:id",
-    name: "volunteerEdit",
-    component: VolunteerEdit,
-    meta: {
-      title: "Medewerker wijzigen",
-    },
-    props: true,
-  },
-  {
-    path: "/registration",
-    name: "campRegistration",
-    component: CampRegistationIndex,
-    meta: {
-      title: "Inschrijvingen",
-    },
-  },
-  {
-    path: "/registration/:id",
-    name: "campRegistrationEdit",
-    component: CampRegistrationEdit,
-    meta: {
-      title: "Inschrijving wijzigen",
-    },
-    props: true,
-  },
-  {
-    path: "/account/settings",
-    name: "settings",
-    component: SettingsView,
-    meta: {
-      title: "Settings",
-    },
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: SigninView,
-    meta: {
-      title: "Log in",
-    },
-  },
-];
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    return savedPosition || { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach((to, from, next) => {
-  document.title = `Stafplicatie ${to.meta.title}`;
-  next();
-});
-
-router.beforeEach((to, from, next) => {
-  const publicPages = ["/login"];
-  const authRequired = !publicPages.includes(to.path);
-  const authStore = useAuthStore();
-
-  if (authRequired && !authStore.isAuthenticated) {
-    return next("/login");
-  } else {
-    next();
-  }
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
 });
 
 export default router;
